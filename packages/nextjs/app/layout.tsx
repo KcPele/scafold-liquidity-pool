0379d34abeb7a7e7e86add3373aeb3b79affff09
KcPele <fidekg123@gmail.com>
2024-04-27 10:45:55 +0100

    next script added

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Script from "next/script";
 import "@rainbow-me/rainbowkit/styles.css";
 import { Toaster } from "react-hot-toast";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
@@ -53,6 +54,39 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
       <body>
+        <Script src="/js/jquery-3.3.1.js"></Script>
+        <Script src="/js/js-plugins/leaflet.js"></Script>
+        <Script src="/js/js-plugins/MarkerClusterGroup.js"></Script>
+        <Script src="/js/js-plugins/crum-mega-menu.js"></Script>
+        <Script src="/js/js-plugins/waypoints.js"></Script>
+        <Script src="/js/js-plugins/jquery-circle-progress.js"></Script>
+        <Script src="/js/js-plugins/segment.js"></Script>
+        <Script src="/js/js-plugins/bootstrap.js"></Script>
+        <Script src="/js/js-plugins/imagesLoaded.js"></Script>
+        <Script src="/js/js-plugins/jquery.matchHeight.js"></Script>
+        <Script src="/js/js-plugins/jquery-countTo.js"></Script>
+        <Script src="/js/js-plugins/Headroom.js"></Script>
+        <Script src="/js/js-plugins/jquery.magnific-popup.js"></Script>
+        <Script src="/js/js-plugins/popper.min.js"></Script>
+        <Script src="/js/js-plugins/particles.js"></Script>
+        <Script src="/js/js-plugins/perfect-scrollbar.js"></Script>
+        <Script src="/js/js-plugins/jquery.datetimepicker.full.js"></Script>
+        <Script src="/js/js-plugins/svgxuse.js"></Script>
+        <Script src="/js/js-plugins/select2.js"></Script>
+        <Script src="/js/js-plugins/smooth-scroll.js"></Script>
+        <Script src="/js/js-plugins/sharer.js"></Script>
+        <Script src="/js/js-plugins/isotope.pkgd.min.js"></Script>
+        <Script src="/js/js-plugins/ajax-pagination.js"></Script>
+        <Script src="/js/js-plugins/swiper.min.js"></Script>
+        <Script src="/js/js-plugins/material.min.js"></Script>
+        <Script src="/js/js-plugins/orbitlist.js"></Script>
+        <Script src="/js/js-plugins/highstock.js"></Script>
+        <Script src="/js/js-plugins/bootstrap-datepicker.js"></Script>
+        <Script src="/js/js-plugins/TimeCircles.js"></Script>
+        <Script src="/js/js-plugins/ion.rangeSlider.js"></Script>
+        <Script defer src="fonts/fontawesome-all.js"></Script>
+        <Script src="/js/main.js"></Script>
+        <Script src="/js/svg-loader.js"></Script>
         <ThemeProvider enableSystem>
           <ScaffoldEthAppWithProviders>
             <CONTEXT_Provider>
