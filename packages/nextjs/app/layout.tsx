ddbc1a8d2fcf22d81a9f555b1d751f1e07e6992b
KcPele <fidekg123@gmail.com>
2024-04-27 10:41:54 +0100

    removed script

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import "@rainbow-me/rainbowkit/styles.css";
-import { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import { CONTEXT_Provider } from "~~/context";
 import "~~/styles/globals.css";
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
-const imageUrl = `${baseUrl}/thumbnail.jpg`;
+// const baseUrl = process.env.VERCEL_URL
+//   ? `https://${process.env.VERCEL_URL}`
+//   : `http://localhost:${process.env.PORT || 3000}`;
+// const imageUrl = `${baseUrl}/thumbnail.jpg`;
 
-const title = "Scaffold-ETH 2 App";
-const titleTemplate = "%s | Scaffold-ETH 2";
-const description = "Built with 🏗 Scaffold-ETH 2";
+// const title = "Scaffold-ETH 2 App";
+// const titleTemplate = "%s | Scaffold-ETH 2";
+// const description = "Built with 🏗 Scaffold-ETH 2";
 
 // export const metadata: Metadata = {
 //   metadataBase: new URL(baseUrl),
@@ -62,39 +61,6 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
             </CONTEXT_Provider>
           </ScaffoldEthAppWithProviders>
         </ThemeProvider>
-        <script src="js/jquery-3.3.1.js"></script>
-        <script src="js/js-plugins/leaflet.js"></script>
-        <script src="js/js-plugins/MarkerClusterGroup.js"></script>
-        <script src="js/js-plugins/crum-mega-menu.js"></script>
-        <script src="js/js-plugins/waypoints.js"></script>
-        <script src="js/js-plugins/jquery-circle-progress.js"></script>
-        <script src="js/js-plugins/segment.js"></script>
-        <script src="js/js-plugins/bootstrap.js"></script>
-        <script src="js/js-plugins/imagesLoaded.js"></script>
-        <script src="js/js-plugins/jquery.matchHeight.js"></script>
-        <script src="js/js-plugins/jquery-countTo.js"></script>
-        <script src="js/js-plugins/Headroom.js"></script>
-        <script src="js/js-plugins/jquery.magnific-popup.js"></script>
-        <script src="js/js-plugins/popper.min.js"></script>
-        <script src="js/js-plugins/particles.js"></script>
-        <script src="js/js-plugins/perfect-scrollbar.js"></script>
-        <script src="js/js-plugins/jquery.datetimepicker.full.js"></script>
-        <script src="js/js-plugins/svgxuse.js"></script>
-        <script src="js/js-plugins/select2.js"></script>
-        <script src="js/js-plugins/smooth-scroll.js"></script>
-        <script src="js/js-plugins/sharer.js"></script>
-        <script src="js/js-plugins/isotope.pkgd.min.js"></script>
-        <script src="js/js-plugins/ajax-pagination.js"></script>
-        <script src="js/js-plugins/swiper.min.js"></script>
-        <script src="js/js-plugins/material.min.js"></script>
-        <script src="js/js-plugins/orbitlist.js"></script>
-        <script src="js/js-plugins/highstock.js"></script>
-        <script src="js/js-plugins/bootstrap-datepicker.js"></script>
-        <script src="js/js-plugins/TimeCircles.js"></script>
-        <script src="js/js-plugins/ion.rangeSlider.js"></script>
-        <script defer src="fonts/fontawesome-all.js"></script>
-        <script src="js/main.js"></script>
-        <script src="js/svg-loader.js"></script>
       </body>
     </html>
   );
